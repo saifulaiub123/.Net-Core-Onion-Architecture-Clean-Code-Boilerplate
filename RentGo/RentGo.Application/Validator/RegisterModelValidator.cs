8af3288d34524da40a3c6f24d93bc384704c4c52
Saiful Islam <saifulprogrammer@gmail.com>
2022-04-19 10:22:29 +0600

    added fluent validation for register model

---
+++ RentGo/RentGo.Application/Validator/RegisterModelValidator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using RentGo.Domain.Model;
+
+namespace RentGo.Application.Validator
+{
+    public class RegisterModelValidator : AbstractValidator<RegisterModel>
+    {
+        public RegisterModelValidator()
+        {
+            RuleFor(x => x.MobileNumber)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Mobile number must not be empty");
+            RuleFor(x => x.Code)
+                .NotNull()
+                .NotEmpty()
+                .Length(4, 4)
+                .WithMessage("Invalid length of code");
+        }
+
+        private void test()
+        {
+            
+        }
+    }
+}
